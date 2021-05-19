ad980bd9694072f797ce211c7dbd268011985be6
aliMhrez <alidman0@hotmail.com>
2021-05-19 14:20:44 +0300

    Adding Student Status Model-Migration-Controller

---
+++ database/migrations/2021_05_19_111328_create_student_statuses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_statuses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->constrained();
+            $table->integer('decision_num')->nullable();
+            $table->date('year');
+            $table->foreignId('status_code_id')->constrained();
+            $table->foreignId('education_year_code_id')->constrained();
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_statuses');
+    }
+}
